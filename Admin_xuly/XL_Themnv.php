5d2a88e3a225e94eaa6625d1fe8886a85f1319ff
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-07 23:20:20 +0700

    Update XL_Themnv.php

--- Admin_xuly/XL_Themnv.php
+++ Admin_xuly/XL_Themnv.php
@@ -3,7 +3,7 @@ include("../connect.php");
 $hoten = trim($_POST['hoten']);
 $CMND =$_POST['cmnd'];
 $diachi =trim($_POST['diachi']);
-$chucvu =trim($_POST['chucvu']);
+$congviec =trim($_POST['congviec']);
 $ngaysinh =trim( $_POST['ngaysinh']);
 $sdt = $_POST['sdt'];
 $gioitinh =$_POST['gioitinh'];
@@ -17,14 +17,14 @@ $img_url = $path.$hinhanh;
 // them tai khoan
 
 
- $sql = "insert into Nhanvien(Hoten_NV,NgaysinhNV,Gt,CMND_NV,DiaChi_NV,SDT_NV,Chucvu,Hinhanh) values('$hoten','$ngaysinh','$gioitinh','$CMND','$diachi',
-                  '$sdt','$chucvu','$img_url')";
+ $sql = "insert into Nhanvien(Hoten_NV,NgaysinhNV,Gt,CMND_NV,DiaChi_NV,SDT_NV,Congviec,Hinhanh) values('$hoten','$ngaysinh','$gioitinh','$CMND','$diachi',
+                  '$sdt','$congviec','$img_url')";
 if(mysqli_query($conn,$sql)){
   if($usr !='' && $pwd!=''){
     $sql = "call ThemTK('$usr','$pwd','$CMND')";
     mysqli_query($conn,$sql);
   }
-   
+
     header("Location:Admin.php?d=themnv");
 
 }else
