88730cbcfa9b569d6cbe6553820a7fb04011fe60
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-07 23:20:41 +0700

    Update Pro_func.sql

--- Pro_func.sql
+++ Pro_func.sql
@@ -97,7 +97,7 @@ begin
         if(exists (select username from taikhoan where MSNV = ma))  then
 				set usr = (select username from taikhoan where MSNV = ma);
         end if;        
-		select usr,MSNV,Hoten_NV,NgaysinhNV,Gt,CMND_NV,DiaChi_NV,SDT_NV,Chucvu,Ngayvaolam,Hinhanh from Nhanvien
+		select usr,MSNV,Hoten_NV,NgaysinhNV,Gt,CMND_NV,DiaChi_NV,SDT_NV,Congviec,Ngayvaolam,Hinhanh from Nhanvien
         where Nhanvien.MSNV = ma;
          
 end/
@@ -130,5 +130,67 @@ delimiter ;
         join Loaiphong on Phong.Maloai = Loaiphong.Maloai
         where MSP = maphong;
     end\
- 
  delimiter ;
+ 
+ 
+ 
+ 
+ 
+ 
+-- procedure Themkhachhang vao bang khach hang va chi tiet dat phong
+delimiter \
+create procedure Themkhach(hoten varchar(30),ngaysinh date, gioitinh varchar(3),cmnd char(9),diachi varchar(100), sdt char(10),
+							email varchar(50))
+		begin 
+				declare makhach int;
+                if(exists (select MaKH from Khachhang_temp where TenKH=hoten)) then
+					set makhach = (select MaKH from Khachhang_temp where TenKH=hoten);
+					insert into Khachhang  values(makhach,hoten,ngaysinh,gioitinh,cmnd,diachi,sdt,email);
+                    end if;
+        end\
+delimiter ;
+drop procedure Datphong;
+drop procedure Themkhach;
+
+delimiter \
+create procedure Datphong(hoten varchar(30),ngaysinh date, gioitinh varchar(3),cmnd char(9),diachi varchar(100), sdt char(10),
+							email varchar(50),maphong int, ngayden date, ngaydi date, songay int)
+		begin 
+				declare makhach int;
+                if(exists (select MaKH from Khachhang_temp where TenKH=hoten)) then
+					set makhach = (select MaKH from Khachhang_temp where TenKH=hoten);
+					insert into Khachhang(MSKH,HoTenKhach,NgaysinhKH,Gioitinh,CMND,DiaChi,SDT,Email)  values(makhach,hoten,ngaysinh,gioitinh,cmnd,diachi,sdt,email);
+                    insert into Datphong(MSP,MSKH,Check_in,Check_out,Sodem) values(maphong,makhach,ngayden,ngaydi,songay);
+                    delete from Khachhang_temp where MaKH=makhach;
+                    end if;
+        end\
+delimiter ;
+drop procedure Datphong;
+call Datphong('TT','2021-07-16','nữ','312463198','TG','0949760662','mail',52,'2021-07-16','2021-07-16',3);
+
+-- function kiemtra phong co trong hay khong
+
+delimiter \
+	create function Kiemtraphongtrong( maphong int,ngayden date, ngaydi date)
+    returns int
+    begin
+			declare d_in date;
+            declare d_out date;
+			if(exists (select Check_in,Check_out from Chitiet where MSP=maphong and Trangthai=1))
+            then 
+				set d_in = (select Check_in from Chitiet where MSP=maphong and Trangthai=1);
+                set d_out = (select Check_in from Chitiet where MSP=maphong and Trangthai=1);
+                if ( (d_in between ngayden and ngaydi) || (d_out between ngayden and ngaydi)) 
+                then 
+					return 0;
+				else 
+					return 1;
+				end if;
+			 else 
+				return 1;
+            end if;
+    end\
+
+delimiter ;
+drop function Kiemtraphongtrong;
+select Kiemtraphongtrong(22,'2021-08-21','2021-08-25');
