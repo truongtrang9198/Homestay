1d2dee89953cb3c62ccd915a6cd7d44a9aebf56b
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-07 23:19:20 +0700

    Update Trangchu1.php

--- Khachhang/Trangchu1.php
+++ Khachhang/Trangchu1.php
@@ -3,8 +3,88 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <style media="screen">
+      #xemngay{
+        width: 120px !important;
+      }
+    </style>
   </head>
   <body>
-    
+    <div class="carousel slide" id="Anhphong" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="Anhphong" data-slide-to="0" class="active"></li>
+        <li data-target="Anhphong" data-slide-to="1"  ></li>
+        <li data-target="Anhphong" data-slide-to="2"  ></li>
+        <li data-target="Anhphong" data-slide-to="3"  ></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="Anhphong/home.jpg" class="d-md-block " alt="" width="960px" height="720px">
+        </div>
+        <div class="carousel-item">
+          <img src="Anhphong/p1.jpg" class="d-md-block " alt="" width="960px" height="720px">
+        </div>
+        <div class="carousel-item">
+          <img src="Anhphong/p2.jpg" class="d-md-block " alt="" width="960px" height="720px">
+        </div>
+        <div class="carousel-item">
+          <img src="Anhphong/p3.jpg" class="d-md-block " alt="" width="960px" height="720px">
+        </div>
+
+      </div>
+      <a href="#Anhphong" class="carousel-control-next" role="button" data-slide="next" >
+        <i class="fas fa-chevron-right" style="background-color:gray"></i>
+      </a>
+    </div>
+        <div class="jumbotron">
+          <h1 class="display-4">Hi, </h1>
+          <p class="lead">Đã đến Cần Thơ du lịch tại sao bạn lại không thử ghé qua Moon's Home?</p>
+          <hr class="my-4">
+          <!-- my-4 -> top-bottom 1.5 -->
+          <p>Với không gian xanh hòa mình với thiên nhiên, như thổi làn gió mát vào tâm hồn, xóa tan cảm giác mệt nhoài nơi ồn ào phố thị. Mang lại cho bạn những phút giây thư giãn thực thụ
+          bên người thân và bạn bè. </p>
+          <p>Vị trí di chuyển thuận lợi, cho bạn dễ dàng du hí khắp nội ô Cần Thơ. Gần các khu vui chơi, mua sắm như Vincom Plaza, ẩm thực đường Đề Thám. Hóng gió chiều ở bến Ninh Kiều, ẩm thực sông nước dân dã với chợ Nổi Cái Răng.
+          Ghé qua chùa Ông thắp nén nhang cầu cho gia đình bình an, hay xa hơn nữa là Thiền Viện Trúc Lâm nổi tiếng khắp miền Tây Nam Bộ.</p>
+          <p>Ngoài những thuận lợi về vị trí, Moon'sHome còn: </p>
+          <ul>
+            <li>Có bãi đỗ xe hoàn toàn miễn phí và an toàn</li>
+            <li>Hệ thống phòng đa dạng</li>
+            <li>Sạch sẽ thoáng mát</li>
+            <li>Trang bị đầy đủ tiện nghi</li>
+            <li>Bếp chung đầy đủ đồ gia dụng</li>
+            <li>Giá cả hợp lý</li>
+            <li>Nhiều ưu đãi đặc biệt</li>
+            <li class="text-danger">Đặc biệt có view siêu ảo tha hồ sống đẹp</li>
+          </ul>
+
+          <a href="Trangchu.php?d=xemphong" class="btn btn-info nav-link text-nowrap" id="xemngay" role="button">XEM NGAY</a>
+        </div>
+        <!-- hien thi quy dinh o homestay -->
+        <div class="card">
+          <div class="card-header">
+            <a href="#Noiqui" class="text-warning card-link" data-toggle="collapse" id="Xemnoiqui">QUY ĐỊNH HOMESTAY</a>
+          </div>
+          <div class="collapse" id="Noiqui" >
+            <div class="card-body">
+              <ol>
+                <li>Quý khách vui lòng xuất trình hộ chiếu hoặc chứng minh nhân dân để làm thủ tục nhận phòng tại Lễ tân.</li>
+                <li> Homestay chỉ chịu trách nhiệm với những tài sản hoặc tiền được gửi tại quầy Lễ tân.</li>
+                <li>Không mang súng đạn, chất cháy nổ, chất độc hại, các chất gây nghiện, không nấu nướng trong phòng.</li>
+                <li>Không thay đổi, di chuyển đồ đạc trong phòng hoặc từ phòng này sang phòng khác. Trường hợp tài sản, đồ dùng trong phòng bị mất, hỏng do chủ quan Quý khách sẽ phải bồi thường.</li>
+                <li>Xin vui lòng không thay đổi phòng hoặc đưa thêm người vào phòng khi chưa đăng ký trước với Lễ tân.</li>
+                <li> Khi ra khỏi phòng, Quý khách vui lòng tắt quạt/đèn, đóng cửa sổ, rút chìa khoá ra khỏi cửa và gửi tại quầy Lễ tân.</li>
+                <li>Trước khi rời khỏi khách sạn, xin Quý khách vui lòng thanh toán toàn bộ các hoá đơn và trả lại chìa khoá phòng cho Lễ tân.</li>
+
+              </ol>
+              <p class="text-muted">Trân trọng!</p>
+            </div>
+          </div>
+        </div>
+    </div>
   </body>
 </html>
+<script type="text/javascript">
+$('.carousel').carousel({
+interval: 2000
+})
+</script>
