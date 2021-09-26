e26d37d57c9a19ac4ff323a2bf5614f37464130c
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-26 09:31:21 +0700

    Create KT_ThemNV.php

---
+++ Admin_xuly/KT_ThemNV.php
@@ -0,0 +1,32 @@
+<?php
+include("../connect.php");
+
+$CMND =$_POST['cmnd'];
+$sdt = $_POST['sdt'];
+
+
+
+
+$sql = "select * from NhanVien";
+$query = mysqli_query($conn,$sql);
+if(mysqli_num_rows($query) != 0){
+  while ($row = mysqli_fetch_array($query)) {
+  if($sdt== $row['SDT_NV'] ){
+    echo("Số điện thoại đã tồn tại!");
+    die();
+  }
+  if ($CMND == $row['CMND_NV']) {
+    echo("CMND/CCCD đã tồn tại!");
+    die();
+  }
+  }
+}
+
+
+echo("2");
+
+
+
+
+
+ ?>
