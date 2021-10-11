3767124f7728f3623755b6e1cd30974098a93bf0
truongtrang9198 <truongtrang9198@gmail.com>
2021-10-11 22:41:51 +0700

    Update DS_loai.php

--- Admin_xuly/DS_loai.php
+++ Admin_xuly/DS_loai.php
@@ -12,8 +12,18 @@ include("../connect.php");
 </head>
 
 <body>
-  <h3>Danh sách loại phòng</h3>
-  <table class="table table-striped table-hover">
+  <nav class="navbar navbar-expand-md ">
+    <ul class="navbar-nav mr-auto">
+      <span class="" style="font-size:30px; color:#1D5B8F;">Danh sách loại phòng</span>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <form class="form-inline" action="#" method="get">
+        <input type="text" class="form-control mr-sm-2" id="Timkiem" name="" value="" placeholder="Search ...">
+        <button type="submit" class="btn btn-primary" name="button"><i class="fas fa-search"></i> </button>
+      </form>
+    </ul>
+  </nav>
+  <table class="table table-striped table-hover" id="table">
     <thead>
       <tr>
         <th scope="col">STT</th>
@@ -53,3 +63,16 @@ include("../connect.php");
 </body>
 
 </html>
+<script type="text/javascript">
+  $(document).ready(function() {
+    //  tìm kiếm nv
+    $('#Timkiem').on('keyup', function(event) {
+      event.preventDefault();
+      var key = $('#Timkiem').val().toLowerCase();
+      $('#table tr').filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(key) > -1);
+      });
+    });
+
+  })
+</script>
