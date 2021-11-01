4c7c4a2cfc3e3109b80a8f643fdc3a907bcb7ee5
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-01 07:47:03 +0700

    Update Datphong.php

--- Khachhang/Datphong.php
+++ Khachhang/Datphong.php
@@ -1,118 +1,150 @@
 <?php
 
-    include("../connect.php");
-
+      if(isset($_POST['maphong']) && isset($_POST['ngaydi']) && isset($_POST['ngayden'])
+                                 && isset($_SESSION['MSKH'])){
+        $maphong = $_POST['maphong'];
+        $ngayden = $_POST['ngayden'];
+        $ngaydi = $_POST['ngaydi'];
+        $makhach = $_SESSION['MSKH'];
+        ob_start();
  ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
 
-    <script type="text/javascript" src="Datphong.js"></script>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <style media="screen">
 
-<script src="Thoigian.js" charset="utf-8"></script>
-  </head>
+      .datphong{
+        /* border: 1px solid black; */
+        /* margin-bottom: 100px; */
+        background-color:#fafafa;
+      }
+      #thongtinkhach{
+        /* margin-left: 50px; */
+        border-right: 2px solid black;
+        /* border-left: 2px solid black; */
 
+      }
+      #button-datphong{
+        margin-right: 20px;
+      }
+    </style>
 
-<body>
-    <h2 class="text-success text-center">Thông tin  đặt phòng</h2>
-    <form class="form-group" action="Trangchu.php?d=xacnhan" method="post">
-    <div class="row">
-      <div class="col-lg-4 md-4">
+  <br>
+  <h2 class="text-dark text-center">Thông Tin Đặt Phòng</h2> <br>
+  <div class="container-fluid">
 
-      <label for="hoten">Họ tên</label>
-      <input type="text" name="hoten" class="form-control" id="hoten" value="" required>
-      <label for="ngaysinh">Ngày sinh</label>
-      <input type="date" name="ngaysinh" id="ngaysinh" value="" class="form-control" required>
-        <!-- gioi tinh -->
-        <div class="form-check form-check-inline">
-          <label for="gioitinh" class="form-check-label" >Giới tính:  </label>
-        </div>
+  <div class="datphong">
 
-        <div class="form-check form-check-inline">
-          <label class="form-check-label"> <input class="form-check-input" type="radio" name="gioitinh" id="nu" value="nữ">Nữ</label>
-        </div>
-        <div class="form-check form-check-inline">
-            <label class="form-check-label"><input  class="form-check-input" type="radio" name="gioitinh" id="nam" value="nam">Nam</label>
-        </div>
-        <!--  -->
+    <div class="row" id=" ">
+      <div class="col-2" id="thongtinkhach">
         <br>
-        <label for="cmnd">CMND</label>
-        <input type="text" name="cmnd" class="form-control" value="" required pattern="[0-9]{9}">
-        <label for="sdt">Số điện thoại</label>
-        <input type="text" name="sdt" class="form-control" value="" required length='10' pattern="[0]?[0-9]{10,11}">
-        <label for="diachi">Địa chỉ</label>
-        <input type="text" name="diachi" class="form-control"value="" required>
-        <label for="email">Email</label>
-        <input type="mail" name="email" id="email" class="form-control" value="">
-        <label for="ngayden">Ngày check in</label>
-        <!-- <input type="date" name="ngayden" id="ngayden" class="form-control" value="" required> -->
-        <input type="text" name="ngayden" id="ngayden" value="" class="form-control">
-        <label for="ngaydi">Ngay check out</label>
-        <!-- <input type="date" name="ngaydi" id="ngaydi" class="form-control" value="" required> <br> -->
-        <input type="text" name="ngaydi" id="ngaydi" value="" class="form-control">
+          <!-- <p><span class="text-muted font-italic">Mã khách: </span><span class="text-center" ></span></p> -->
+          <input type="text" id="makhach" name="" value="{makhach}" class="form-control-plaintext" hidden>
+          <p><span class="text-muted font-italic">Họ tên khách: </span><span class="text-center"> <b>{Hoten}</b> </span></p>
 
-        <input type="number" name="songay" value="" class="form-control" id="songay" hidden>
-        <!-- Thong bao doc noi quy homestay -->
-        <p  style="font-size:10px;"><span class="text-muted">\*Vui lòng đọc 'Nội quy homestay' trước khi đặt phòng</span>
-        <a href="Trangchu.php?d=Trangchu/#Xemnoiqui" class="text-info">Nội quy honestay</a> </p>
-        <div class="row" id="button">
-        &ensp;&ensp;  <button type="button" name="huy" class="btn1 btn btn-primary" id="huy">Hủy</button> &ensp;
-          <button type="submit" name="tieptuc" class="btn1 btn btn-primary" id="tieptuc">Tiếp tục</button>
-        </div>
+          <p><span class="text-muted font-italic"> Ngày sinh: </span><span class="text-center">{Ngaysinh}</span></p>
+          <p><span class="text-muted font-italic">Giới tính: </span> <span class="text-center">{Gioitinh}</span></p>
 
+          <p><span class="text-muted font-italic">CMND: </span><span class="text-center">{CMND}</span></p>
+
+          <p><span class="text-muted font-italic">Số điện thoại: </span><span class="text-center">{sdt}</span></p>
+
+          <p><span class="text-muted font-italic">Email: </span><span class="text-center">{email}</span></p>
+
+          <p><span class="text-muted font-italic">Địa chỉ: </span><span class="text-center">{Diachi}</span></p>
+        <br>
     </div>
-    <div class="col-lg-6 md-6 offset-1">
+    <div class="col-10">
       <div class="row">
-      <!-- truy van phong -->
-      <label for="phong">Phòng</label>
-      <select class="form-control" name="phong" id="phong">
-        <option value="">Chọn phòng</option>
-      <?php
+      <table class="table" id="table-phongdat">
+        <thead>
+          <tr>
+            <th>MSP</th>
+            <th>Tên Phòng</th>
+            <th>Giá/đêm</th>
+            <th>Check in</th>
+            <th>Check out</th>
+            <th>Số lượng (đêm)</th>
+            <th>Thành giá</th>
+            <th>Mô tả phòng</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td >{MSP}</td>
+            <td>{Tenphong}</td>
+            <td>{Gia}</td>
+            <td >{Ngayden} </td>
+            <td >{Ngaydi}</td>
+            <td >{Soluong} </td>
+            <td >{Thanhgia1}</td>
+            <td>{Mota}</td>
+            <input type="text" id="maphong" name="" value="{MSP}" class="form-control-plaintext" hidden>
+            <input type="text" name="" value="{Thanhgia}" id="thanhgia" class="form-control-plaintext" hidden >
+            <input type="text" name="" id="Ngayden" value="{ngayden}" class="form-control-plaintext" hidden>
+            <input type="text" name="" id="Ngaydi" value="{ngaydi}" class="form-control-plaintext" hidden>
+            <input type="text" name="" id="sodem" value="{Soluong}" class="form-control-plaintext" hidden>
+            <input type="text" name="" value="{Thanhgia1}"  class="form-control-plaintext" hidden>
+          </tr>
+        </tbody>
+      </table>
+       </div>
 
-          $sql = "select MSP,Tenphong from Phong";
-          $query = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($query) != 0) {
-          while ($row = mysqli_fetch_array($query)) {
-       ?>
-       <option value="<?php echo $row['MSP']; ?>"> <?php echo $row['Tenphong']; ?> </option>
-    <?php
+     </div>
+     </div>
+
+</div>
+<div class="row" id="button-datphong">
+  &ensp;&ensp; <button type="button" name="huy" class="btn1 btn btn-primary" onclick="window.history.back();" id="huy">Hủy</button> &ensp;
+  <button type="submit" name="btn-datphong" class="btn1 btn btn-primary" id="btn-datphong">Đặt phòng</button>
+</div>
+</div>
+<br>
+<?php
+    $s = ob_get_clean();
+    // lấy thông tin khách hàng
+    include("../connect.php");
+    $sql = "SELECT*from Khachhang where MSKH='$makhach'";
+    if(mysqli_query($conn,$sql)){
+      $row=mysqli_fetch_array(mysqli_query($conn,$sql));
+      $s = str_replace("{Hoten}",$row['HoTenKhach'],$s);
+      $s = str_replace("{Ngaysinh}",$row['NgaysinhKH'],$s);
+      $s = str_replace("{Gioitinh}",$row['Gioitinh'],$s);
+      $s = str_replace("{CMND}",$row['CMND'],$s);
+      $s = str_replace("{sdt}",$row['SDT'],$s);
+      $s = str_replace("{Diachi}",$row['DiaChi'],$s);
+      $s = str_replace("{email}",$row['Email'],$s);
     }
-  } else {
-    echo "Không truy vấn được!";
-  }
-    ?>
-</select>
-</div>
-<div class="row">
-      <a href="#" data-toggle="collapse" data-target="#thongtinphong" id="laythongtin" class="nav-link">
-      <span class="text-warning"> Thông tin phòng</span> </a>
-      <div class="collapse" id="thongtinphong">
-        <p class="text-muted" id="thongbao"></p>
-        <!-- <p id="thongtinphong"></p> -->
-        <script type="text/javascript">
-            $(document).ready(function(){
-              $('#laythongtin').click(function(){
-                var maphong = $('#phong').val();
-                if(maphong==''){
-                  $('#thongbao').html('Không có thông tin.');
-                }else{
-                  $.get("Laythongtinphong.php",{maphong:maphong},function(data){
-                      $('#thongtinphong').html(data);
-                  });
-              }
-            });
-          });
-        </script>
-      </div>
-</div>
-    </div>
-    </div>
-    </form>
-  </body>
+  $s = str_replace("{makhach}",$makhach,$s);
 
-</html>
-<!-- kiem tra thong tin ma phong va thoi gian neu thoi gian do phong co trong chi tiết HD thì hong duoc dat phong -->
+  //  $soluong = ($ngaydi-$ngayden);
+  include("../connect.php");
+  $ngayden_1 = date("Y-m-d",strtotime($ngayden));
+  $ngaydi_1 = date("Y-m-d",strtotime($ngaydi));
+  // Hiển thị giá trị check in, check out
+  $s = str_replace("{Ngayden}",$ngayden,$s);
+  $s = str_replace("{Ngaydi}",$ngaydi,$s);
+  // Giá trị check in, check out
+  $s = str_replace("{ngayden}",$ngayden_1,$s);
+  $s = str_replace("{ngaydi}",$ngaydi_1,$s);
+ //  lấy thông tin phòng
+  $sql = "call Phongdat(?,?,?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("sss",$maphong,$ngayden_1,$ngaydi_1);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  while ($row = $result->fetch_assoc()) {
+    $s = str_replace("{MSP}",$maphong,$s);
+    $s = str_replace("{Tenphong}",$row['TenPhong'],$s);
+    $s = str_replace("{Soluong}",$row['songay'],$s);
+    $s = str_replace("{Mota}",$row['Mota'],$s);
+    $s = str_replace("{Gia}",number_format($row['Gia']),$s);
+    $s = str_replace("{Thanhgia1}",number_format($row['Gia']*$row['songay']),$s);
+    $s = str_replace("{Thanhgia}",$row['Gia']*$row['songay'],$s);
+
+}
+    echo $s;
+
+}else{
+   echo ("Không nhận được thông tin!");
+
+}
+ ?>
