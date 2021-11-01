d99c6cb1d296969210bb899ecb87986fb045fc61
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-01 07:47:44 +0700

    Create Thanhtoan.php

---
+++ Khachhang/Thanhtoan.php
@@ -0,0 +1,9 @@
+<div class="jumbotron">
+  <h1 class="display-4">Hướng dẫn thanh toán online</h1>
+  <p class="lead">Sau khi quý khách đăng ký đặt phòng thành công trên Website
+                  sau đó thanh toán tiền phòng qua <br>
+                  <b>STK: xxxxx001 Chủ sở hữu XXXX</b>  <br> Chụp ảnh chuyển khoản gửi qua email
+                  Moonhomestay@gmail.com để hoàn thành thủ tục đặt phòng.</p>
+    <p>Mọi thắc mắc xin vui lòng liên hệ qua hotline: 00000001 để được hỗ trợ sớm nhất! </p>
+    <p><b>Xin cảm ơn!</b> </p>
+</div>
