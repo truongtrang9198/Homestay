a10f8f58458719ba537660323fb6687878dde2bb
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-01 07:48:48 +0700

    Update Xulydatphong.php

--- Khachhang/Xulydatphong.php
+++ Khachhang/Xulydatphong.php
@@ -1,34 +1,24 @@
 <?php
 include("../connect.php");
 
-$hoten = $_POST['hoten'];
-$ngaysinh = $_POST['ngaysinh'];
-$gioitinh = $_POST['gioitinh'];
-$sdt = $_POST['sdt'];
-$email = $_POST['email'];
-$cmnd = $_POST['cmnd'];
-$diachi = $_POST['diachi'];
+$makhach = $_POST['makhach'];
+$maphong = $_POST['maphong'];
 $ngayden = $_POST['ngayden'];
 $ngaydi = $_POST['ngaydi'];
-$maphong = $_POST['maphong'];
-$songay = $_POST['songay'];
+$sodem = $_POST['sodem'];
+$thanhgia = $_POST['thanhgia'];
+$trangthai = $_POST['trangthai'];
 
 // up len server
-$sql = "call Datphong('$hoten','$ngaysinh','$gioitinh','$cmnd','$diachi','$sdt',
-                        '$email','$maphong','$ngayden','$ngaydi','$songay')";
+    // echo $ngayden;
+ $sql = "insert into Datphong(MSP,MSKH,Check_in,Check_out,Sodem,Tienphong,Trangthai)
+         values('$maphong','$makhach','$ngayden','$ngaydi','$sodem','$thanhgia','$trangthai')";
+//$stmt->execute();
 if(mysqli_query($conn,$sql)){
   echo("true");
 }else{
   echo "Lỗi truy vấn!";
-
 }
-
-
-
-
-
-
-
 
 
  ?>
