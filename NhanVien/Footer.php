5a1d6dbac7953f9496224bc58db8d26b4f6ede1c
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-14 01:56:55 +0700

    Create Footer.php

---
+++ NhanVien/Footer.php
@@ -0,0 +1,12 @@
+
+  <div class="footer">
+
+
+    <h5 class="text-light pt-1"><i class="fas fa-calendar-alt"></i> Open: 24/7</h5>
+
+  <p>Mọi thông tin chi tiết xin vui lòng liên hệ: </p>
+  <p><i class="fas fa-home"></i> Moon's Homestay Khu II, đường 3/2, P.Xuân Khánh, Q.Ninh Kiều, TP.Cần Thơ</p>
+  <p><i class="fas fa-mobile-alt"></i> 0949760662</p>
+  <p><i class="fas fa-envelope"></i> Moonhomestay@gmail.com</p>
+  <p><i class="fab fa-facebook-square"></i> Moon's Homestay</p>
+  <p id="loiket">Moon's Homestay rất vui được phục vụ quý khách!</p></div>
