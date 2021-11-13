ef2719a36d6c9782809036c62861792f1c19fed8
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-14 01:56:36 +0700

    Update XulyDangnhap.php

--- NhanVien/XulyDangnhap.php
+++ NhanVien/XulyDangnhap.php
@@ -1,7 +1,15 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
   session_start();
+}
+  include_once("../connect.php");
   $username = $_POST['usr'];
-  $_SESSION['username'] = $username;
-  header("Location:Nhanvien.php?d=trangchu");
+  $sql = "select MSNV from Taikhoan where username='$username'";
+  if(mysqli_query($conn,$sql)){
+    $row=mysqli_fetch_array(mysqli_query($conn,$sql));
+    $_SESSION['username'] = $username;
+    $_SESSION['MSNV'] = $row['MSNV'];
+    header("Location:index.php?d=trangchu");
+  }
 
  ?>
