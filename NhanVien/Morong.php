ef59d813e713a0c62cab5e045f13e5af3b4a6597
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-14 01:56:14 +0700

    Create Morong.php

---
+++ NhanVien/Morong.php
@@ -0,0 +1,20 @@
+<style media="screen">
+  #select-morong{
+    width: 500px !important;
+  }
+</style>
+<br>
+<div class="row">
+  <div class="col-6">
+  <form class="form-inline" action="index.php" method="get">
+    <select class="custom-select" name="d" id="select-morong">
+      <option selected>Choose...</option>
+      <option value="checkin_homnay">Hôm nay</option>
+      <option value="tatca_checkin">Tất cả check in</option>
+      <option value="tt_phong">Tình trạng phòng</option>
+    </select>
+    <button type="submit" class="btn" name="button">Hiển thị</button>
+  </form>
+</div>
+</div>
+<hr>
