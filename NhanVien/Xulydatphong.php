67e07bea341aba1d71d4aac95ad0ef36f49c18e4
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-15 01:06:48 +0700

    Create Xulydatphong.php

---
+++ NhanVien/Xulydatphong.php
@@ -0,0 +1,24 @@
+<?php
+include("../connect.php");
+
+$makhach = $_POST['makhach'];
+$maphong = $_POST['maphong'];
+$ngayden = $_POST['ngayden'];
+$ngaydi = $_POST['ngaydi'];
+$sodem = $_POST['sodem'];
+$thanhgia = $_POST['thanhgia'];
+$trangthai = $_POST['trangthai'];
+
+// up len server
+    // echo $ngayden;
+ $sql = "insert into Datphong(MSP,MSKH,Check_in,Check_out,Sodem,Tienphong,Trangthai,Loaidp)
+         values('$maphong','$makhach','$ngayden','$ngaydi','$sodem','$thanhgia','$trangthai','Online')";
+//$stmt->execute();
+if(mysqli_query($conn,$sql)){
+  echo("true");
+}else{
+  echo "Lỗi truy vấn!";
+}
+
+
+ ?>
